012c5c3b94b5fa2de49d9804e917da3970ab30de
= <ricksarkar2005@gmail.com>
2025-02-15 18:20:00 +0530

    X-Client-App added

--- middlewares/securityMiddleware.js
+++ middlewares/securityMiddleware.js
@@ -9,7 +9,7 @@ const securityMiddleware = (app) => {
         credentials: true,
         optionsSuccessStatus: 200,
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        allowedHeaders: ["Content-Type", "Authorization"],
+        allowedHeaders: ["Content-Type", "Authorization","X-Client-App"],
         exposedHeaders: ["Content-Length", "X-Knowledge-Base"],
         preflightContinue: false,
     }));
