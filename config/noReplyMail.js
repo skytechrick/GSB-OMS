5b5ec898bddfb6125bd8d2f176e80f3413cabc70
= <ricksarkar2005@gmail.com>
2025-02-16 02:18:58 +0530

    Config modified for mail sending and SMPT...

---
+++ config/noReplyMail.js
@@ -0,0 +1,18 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    host: process.env.NO_REPLY_HOST,
+    port: parseInt(process.env.NO_REPLY_PORT , 10),
+    secure: parseInt(process.env.NO_REPLY_PORT , 10) === 465,
+    auth: {
+        user: process.env.NO_REPLY_MAIL_ID,
+        pass: process.env.NO_REPLY_MAIL_PASSWORD,
+    },
+    debug: false,   // console print
+    logger: false,  // console print
+    connectionTimeout: 6000,
+});
+
+export default transporter;
--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,21 +1,4 @@
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
-import { parse } from 'path';
-dotenv.config();
-
-const transporter = nodemailer.createTransport({
-    host: process.env.NO_REPLY_HOST,
-    port: parseInt(process.env.NO_REPLY_PORT , 10),
-    secure: parseInt(process.env.NO_REPLY_PORT , 10) === 465,
-    auth: {
-        user: process.env.NO_REPLY_MAIL_ID,
-        pass: process.env.NO_REPLY_MAIL_PASSWORD,
-    },
-    debug: false,   // console print
-    logger: false,  // console print
-    connectionTimeout: 6000,
-});
-
+import transporter from "../config/noReplyMail"
 export const sendMail = async (mailOptions) => {
     try{
         let info = await transporter.sendMail(mailOptions);
