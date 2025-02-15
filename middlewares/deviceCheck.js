b459d9a2bfb6daab07af9599fb26275599acbf02
= <ricksarkar2005@gmail.com>
2025-02-15 12:14:44 +0530

    Device check updated with more parameters and specifications.

--- middlewares/deviceCheck.js
+++ middlewares/deviceCheck.js
@@ -1,7 +1,44 @@
 
 export default async ( req , res , next ) => {
     try {
-        req.isWeb = req.headers["user-agent"]?.includes("Mozilla");
+        const userAgent = req.headers["user-agent"];
+        
+        const blockedUserAgents = [
+            // "PostmanRuntime",
+            "Insomnia",          // Insomnia API Client
+            "HttpClient",        // Generic HTTP Clients
+            "curl",              // cURL command-line tool
+            "Wget",              // Wget command-line tool
+            "Python-urllib",     // Python urllib
+            "Java",              // Java-based bots
+            "Scrapy",            // Scrapy web scraper
+            "Go-http-client",    // GoLang HTTP client
+        ];
+        
+        if (blockedUserAgents.some(ua => userAgent.toLowerCase().includes(ua.toLowerCase()))) {
+            return res.status(403).send({
+                status: "error",
+                message: "Access denied"
+            });
+        };
+
+        const allowedClients = ["gsb-web", "gsb-android", "gsb-ios"];
+        const clientApp = req.headers["x-client-app"];
+
+        if (!clientApp || !allowedClients.includes(clientApp)) {
+            return res.status(403).json({
+                status: "error",
+                message: "Unauthorized client"
+            });
+        };
+        
+        req.isWeb = false;
+        const isWeb = /\b(Mozilla|Chrome|Safari|Edge|Firefox|Opera)\b/.test(userAgent);
+        
+        if(clientApp === "gsb-web" && isWeb) {
+            req.isWeb = isWeb;
+        };
+
         next();
     } catch (error) {
         next(error);
