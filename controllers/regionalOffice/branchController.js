268b91766b76f2f2321da059b01ad9a1d6a1e74b
= <ricksarkar2005@gmail.com>
2025-02-16 21:26:03 +0530

    Create branch manager and fetch is done.

--- controllers/regionalOffice/branchController.js
+++ controllers/regionalOffice/branchController.js
@@ -1,8 +1,9 @@
 import branch from "../../models/branch.js";
+import branchManager from "../../models/branchManager.js";
 import regionalOffice from "../../models/regionalOffice.js";
 import { hashPassword } from '../../utils/passwordHandler.js';
 import { sendMail } from '../../utils/sendMail.js';
-import { createBranchSchema } from '../../utils/zodSchema.js';
+import { createBranchSchema , createBranchManagerSchema } from '../../utils/zodSchema.js';
 
 export const createBranch = async (req, res) => {
     try {
@@ -86,6 +87,110 @@ export const getAllBranch = async ( req , res , next ) => {
             message: "All branches",
             countDocuments: allBranch.length,
             data: allBranch,
+            meta: {
+                totalCount,
+                limit: parseInt(limit, 10),
+                page: parseInt(page, 10),
+                availablePages: Math.ceil(totalCount / limit),
+                more: (totalCount - ( page * limit )) > 0 ? true : false,
+            }
+        });
+
+    } catch (error) {
+        next(error);
+    };
+};
+
+export const createBranchManager = async ( req , res , next ) => {
+    try {
+        
+        const regionalOfficerData = req.regionalOfficer;
+
+        const validatedData = createBranchManagerSchema.safeParse(req.body);
+
+        if(validatedData.error) {
+            return res.status(400).json({
+                status: "error",
+                message: "Unauthorized Access",
+            });
+        };
+
+        const branchData = await branch.findById(validatedData.data.branchId);
+
+        if(!branchData) {
+            return res.status(404).json({
+                status: "error",
+                message: "Branch not found.",
+            });
+        };
+
+        const branchManagerExist = await branchManager.exists({
+            email: validatedData.data.email
+        });
+
+        if(branchManagerExist) {
+            return res.status(400).json({
+                status: "error",
+                message: "Branch Manager already exist.",
+            });
+        };
+
+        const hashedPassword = await hashPassword(validatedData.data.personalDetails.mobileNumber);
+
+        const newBranchManager = new branchManager({
+            branch: validatedData.data.branchId,
+            personalDetails: validatedData.data.personalDetails,
+            email: validatedData.data.email,
+            password: hashedPassword,
+            role: validatedData.data.role,
+            address: validatedData.data.address,
+        });
+
+        await newBranchManager.save();
+
+        branchData.managers = newBranchManager._id;
+        await branchData.save();
+
+        
+        regionalOfficerData.accountHistory.push({
+            historyType: "branch",
+            about: "Branch manager created : " + newBranchManager.personalDetails.firstName,
+            relation: newBranchManager._id,
+        });
+
+        await regionalOfficerData.save();
+
+        return res.status(201).json({
+            status: "success",
+            message: "Branch Manager created successfully.",
+            data: newBranchManager,
+        });
+
+    } catch (error) {
+        next(error);
+    };
+};
+
+export const getAllBranchManager = async ( req , res , next ) => {
+    try {
+        
+        const {
+            limit = 10,
+            page = 1,
+        } = req.query;
+
+        const allBranchManager = await branchManager.find({})
+        .select("-__v -address -loggedIn -authentication -password -accountHistory")
+        .limit(parseInt(limit, 10))
+        .skip(page > 0 ? ( ( page - 1 ) * limit ) : 0 );
+        
+        const totalCount = await branchManager.find({}).countDocuments();
+
+        return res.status(200).json({
+            status: "success",
+            message: "All Branch managers",
+            countDocuments: allBranchManager.length,
+            data: allBranchManager,
             meta: {
                 totalCount,
                 limit: parseInt(limit, 10),
--- routes/regionalOffice/branchRouter.js
+++ routes/regionalOffice/branchRouter.js
@@ -2,7 +2,9 @@ import express from 'express';
 const branchRouter = express.Router();
 export default branchRouter;
 
-import { createBranch , getAllBranch } from '../../controllers/regionalOffice/branchController.js';
+import { createBranch , getAllBranch , createBranchManager , getAllBranchManager } from '../../controllers/regionalOffice/branchController.js';
 
 branchRouter.use("/create" , createBranch );
 branchRouter.use("/all" , getAllBranch );
+branchRouter.use("/manager/create" , createBranchManager );
+branchRouter.use("/manager/all" , getAllBranchManager );
