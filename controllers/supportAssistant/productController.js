1ff8ba89759cc51410fb14eb559d60dd3ea81418
= <ricksarkar2005@gmail.com>
2025-03-02 01:29:10 +0530

    Get all products by asistant done.

--- controllers/supportAssistant/productController.js
+++ controllers/supportAssistant/productController.js
@@ -217,3 +217,42 @@ export const createProduct = async ( req , res , next ) => {
         next(error);
     };
 };
+
+export const getAllProducts = async ( req , res , next ) => {
+    try {
+
+        const {
+            page = 1,
+            limit = 10,
+        } = req.body;
+
+        const products = await product
+            .find({
+                supportOffice: req.supportAssistantData.supportOffice._id
+
+            })
+            .limit(parseInt(limit, 10))
+            .skip((parseInt(page, 10) - 1) * limit)
+            .exec();
+
+        const totalCount = await product.countDocuments({
+            supportOffice: req.supportAssistantData.supportOffice._id
+        }).exec();
+
+        return res.status(200).json({
+            status: "success",
+            total: products.length,
+            products,
+            meta: {
+                totalCount,
+                limit: parseInt(limit, 10),
+                page: parseInt(page, 10),
+                availablePages: Math.ceil(totalCount / limit),
+                more: (totalCount - ( page * limit )) > 0 ? true : false,
+            }
+        });
+
+    } catch (error) {
+        next(error);
+    };
+};
--- routes/supportAssistant/productController.js
+++ routes/supportAssistant/productController.js
@@ -2,8 +2,9 @@ import express from 'express';
 const productRouter = express.Router();
 export default productRouter;
 
-import { createProduct } from '../../controllers/supportAssistant/productController.js';
+import { createProduct , getAllProducts } from '../../controllers/supportAssistant/productController.js';
 import { uploadProductMiddleware } from '../../middlewares/upload.js';
 import { productImageProcessMiddleWare } from '../../middlewares/productImageProcessMiddleWare.js';
 
 productRouter.post("/create" , uploadProductMiddleware , productImageProcessMiddleWare , createProduct );
+productRouter.post("/all" , getAllProducts );
